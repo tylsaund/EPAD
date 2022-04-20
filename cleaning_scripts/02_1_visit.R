1bc81f3feaca2a1f12ba0eca6149f7d4cbfa4147
tylsaund <61541652+tylsaund@users.noreply.github.com>
2022-04-20 11:11:35 +0100

    added cleaning scripts folder

---
+++ cleaning_scripts/02_1_visit.R
@@ -0,0 +1,9 @@
+# script purpose: cleaning Visits dataframe script. Only necessary if using longitudinal data.
+
+# setup ==========
+library(tidyverse)
+library(here) # for hiding path names
+
+source("01_import.R")
+
+# 
