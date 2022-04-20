1b64084c4ab4221401196560fce865851ec57018
tylsaund <61541652+tylsaund@users.noreply.github.com>
2022-04-20 11:25:42 +0100

    updated visit script to include code to create time variable

--- cleaning_scripts/02_1_visit.R
+++ cleaning_scripts/02_1_visit.R
@@ -6,4 +6,14 @@ library(here) # for hiding path names
 
 source("01_import.R")
 
-# 
+# Create time variable =====
+
+# reformat "Date" as date type
+visits$visdat_int <- as.Date(visits$visdat_int, format = "%Y-%m-%d")
+# make "Time" variable by subtracting visit date from previous visit date per participant
+visits <- visits %>%
+  group_by(patient_id) %>% arrange(patient_id, visit) %>%
+  mutate(time= visdat_int - first(visdat_int))
+# remove "days" from time variable 
+visits$time <- gsub(" days","", visits$time) %>%
+  as.numeric()
