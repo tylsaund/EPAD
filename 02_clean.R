406be13e09c5e577632da603c92bb1c7b6391429
tylsaund <61541652+tylsaund@users.noreply.github.com>
2022-08-18 15:25:25 +0100

    updated with column classes

--- 02_clean.R
+++ 02_clean.R
@@ -17,15 +17,16 @@ source("01_import.R")
 names(visits)[4] <- "day"
 visits$day <- as.Date(visits$day, format = "%Y-%m-%d")
 
-## Make "Time" variable by subtracting visit date from previous visit date per participant
+## Make "Time" variable by subtracting visit date from previous visit date per participant. Time in days, months, and time centered
 visits <- visits %>%
   group_by(patient_id) %>% arrange(patient_id, visit) %>%
   mutate(time= day - first(day), 
          time = as.numeric(time),
-         time_months = time / 30)
+         time_months = time / 30,
+         time_centered = time_months - mean(time_months, na.rm=TRUE))
 
 # Tidy age variable ==========
-#Change Age factor from years/months to just years
+#Change Age factor from years/months to just years and an age centered variable
 create_age <- function(x){
   years <- x$age_years * 12
   months <- x$age_months
@@ -37,6 +38,7 @@ create_age <- function(x){
   x
 }
 socio_demographics <- create_age(socio_demographics)
+socio_demographics$age_centered <- socio_demographics$age - mean(socio_demographics$age, na.rm=TRUE)
 
 # Clean CSF variables =========
 
@@ -52,12 +54,20 @@ mutate_at(vars(c("abeta_1_42_result",
                  "ptau_result",
                  "ttau_result")), as.numeric)
 
+## Create categorical variable with a-beta status
+csf <- csf %>%
+  mutate(abeta_status = case_when(
+    abeta_1_42_result <= 1000 ~ "1",
+    abeta_1_42_result > 1000 ~ "0"))
+
+
 # APOE variable ==========
 apoe <- apoe %>%
   mutate(apoe_e4 = case_when(
     apoe_result == "e4/e4" | apoe_result == "e2/e4" | apoe_result == "e3/e4" ~ "1",
     apoe_result == "e2/e3" | apoe_result == "e2/e2" | apoe_result == "e3/e3" ~ "0"))
 
+         
 # Merge ==========
 df1 <- merge(csf, socio_demographics, by="patient_id") #not the most elegant way of merging, but other methods run into memory issues
 df2 <- left_join(df1, unique(visits))
@@ -68,3 +78,12 @@ df6 <- left_join(df5, unique(dementia_diag))
 df7 <- left_join(df6, unique(mmse))
 df8 <- left_join(df7, unique(rbans))
 epad_merged <- left_join(df8, unique(apoe))
+
+# Convert column classes =====
+epad_merged <- epad_merged %>%
+  mutate(across(c("sex", 
+                  "family_dementia_history", 
+                  "visit_site", 
+                  "apoe_e4",
+                  "abeta_status"), ~ factor(.)))
+
